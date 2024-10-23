f20bc26396fbd818dab46af68dc32dcb574e4810
Kay (Andrew) <chauanhtuan185@gmail.com>
2024-10-23 13:09:10 +0700

    feat : add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="twitter:card" content="player" />
+        <meta name="twitter:site" content="@kayx86" />
+        <meta name="twitter:title" content="Demo Sblinks" />
+        <meta name="twitter:description" content="Brief description of your app" />
+        <meta
+          name="twitter:player"
+          content="https://vite-react-chauanhtuan185s-projects.vercel.app/swap"
+        />
+        <meta name="twitter:player:width" content="360" />
+        <meta name="twitter:player:height" content="560" />
+        <meta
+          name="twitter:image"
+          content="https://app.ekubo.org/pwa-512x512.png"
+        />
+        <meta property="og:url" content="https://vite-react-chauanhtuan185s-projects.vercel.app/swap" />
+        <meta property="og:title" content="test" />
+        <meta property="og:description" content="test" />
+        <meta property="og:image" content="https://app.ekubo.org/pwa-512x512.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
